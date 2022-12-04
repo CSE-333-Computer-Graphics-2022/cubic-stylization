660fe0ed7af028344d5417472d02e0a17daf8472
basp0 <bassampervez@gmail.com>
2022-12-04 14:40:46 +0530

    feat: load mesh during runtime via command line arg, default to bunny if no ffile path specified

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/readOBJ.h>
-
+#include <iostream>
+using namespace std;
+  
 
 int main(int argc, char *argv[])
 {
 
   Eigen::MatrixXd V,TC,CN;
   Eigen::MatrixXi F,FTC,FN;
-
-
-  igl::readOBJ("meshes/bunny.obj", V, TC,CN,F,FTC,FN);
-
+  string arg;
+  if ( argc == 0 || argv[1]==NULL || strlen(argv[1]) == 0 ){
+    cout << "no file path specified, defaulting to bunny" << "\n";
+    arg = "meshes/bunny.obj";
+  }else{
+  arg= argv[1];
+  } 
+  
+  
+  igl::readOBJ(arg, V, TC,CN,F,FTC,FN);
+  
   
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
